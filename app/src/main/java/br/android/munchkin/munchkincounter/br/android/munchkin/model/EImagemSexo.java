a045240a3337b13600bc124e668121324d035307
Julio Cezar Lazzarim <juliolazzarim@softpharma.com.br>
2015-01-29 15:41:29 -0200

    Algumas telas

---
+++ app/src/main/java/br/android/munchkin/munchkincounter/br/android/munchkin/model/EImagemSexo.java
@@ -0,0 +1,12 @@
+package br.android.munchkin.munchkincounter.br.android.munchkin.model;
+
+/**
+ * Created by SOFT0085 on 26/01/2015.
+ */
+public enum EImagemSexo {
+
+    MASCULINO,
+    FEMININO
+        ;
+
+}
---
+++ app/src/main/java/br/android/munchkin/munchkincounter/br/android/munchkin/model/Player.java
@@ -0,0 +1,47 @@
+package br.android.munchkin.munchkincounter.br.android.munchkin.model;
+
+/**
+ * Created by SOFT0085 on 26/01/2015.
+ */
+public class Player {
+
+    String nome;
+    EImagemSexo sexo;
+    Integer vitorias;
+    Integer jogos;
+
+    public Player() {
+    }
+
+    public EImagemSexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(EImagemSexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Integer getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(Integer vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public Integer getJogos() {
+        return jogos;
+    }
+
+    public void setJogos(Integer jogos) {
+        this.jogos = jogos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
